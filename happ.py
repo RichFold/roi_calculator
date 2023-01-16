b83f4a8688d4e2ae1b3873b4e2a58740b935f390
RichFold <richard@folddigital.co.uk>
2023-01-16 18:52:40 +0000

    Update happ.py

--- happ.py
+++ happ.py
@@ -1,23 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-
-def get_ranking(url, keywords):
-    rankings = {}
-    for keyword in keywords:
-        search_url = "https://www.google.com/search?q=" + keyword
-        response = requests.get(search_url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        links = [a['href'] for a in soup.find_all('a', href=True) if a.text]
-        try:
-            rankings[keyword] = links.index(url) + 1
-        except ValueError:
-            rankings[keyword] = "Not found"
-    return rankings
+def calculate_ctr(ranking, searches):
+    ctr = (1 / ranking) * (searches / 100)
+    return ctr
 
 if __name__ == "__main__":
-    url = input("Enter the website URL: ")
-    keywords = input("Enter a list of keywords separated by commas: ").split(",")
-    rankings = get_ranking(url, keywords)
-    for keyword, ranking in rankings.items():
-        print(f"{keyword}: {ranking}")
-
+    ranking = int(input("Enter the website's search engine ranking: "))
+    searches = int(input("Enter the number of searches for the keyword: "))
+    ctr = calculate_ctr(ranking, searches)
+    print(f"CTR: {ctr}")
