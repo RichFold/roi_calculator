27a20eb7c58238da979882e3947cdd58531df2a6
RichFold <richard@folddigital.co.uk>
2024-02-10 12:58:23 +0000

    Delete happ.py

--- happ.py
+++
@@ -1,9 +0,0 @@
-def calculate_ctr(ranking, searches):
-    ctr = (1 / ranking) * (searches / 100)
-    return ctr
-
-if __name__ == "__main__":
-    ranking = int(input("Enter the website's search engine ranking: "))
-    searches = int(input("Enter the number of searches for the keyword: "))
-    ctr = calculate_ctr(ranking, searches)
-    print(f"CTR: {ctr}")
